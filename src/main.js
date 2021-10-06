573cbf0efdefc6e90f48941649f6ce12101578e0
Logan Edmonds <logantedmonds@gmail.com>
2021-10-06 11:48:13 -0500

    Updated vuex and router to vue3 version

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import vSelect from 'vue-select'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(vSelect).mount('#app')
---
+++ src/services/cryptoService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default {
+
+    getCoinPrices() {
+        return axios.get()
+    }
+
+}
