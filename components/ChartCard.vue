772c751939490c5c2e0239880675a15b9daaf31c
jonbiveo <logantedmonds@gmail.com>
2021-12-10 17:02:57 -0600

    added search bar, moved functionality of graph to its own component. almost there

--- components/ChartCard.vue
+++ components/ChartCard.vue
@@ -1,7 +1,22 @@
 <template>
   <v-card min-height="900px" min-width="700px" class="mx-auto">
-    <v-card-title class="justify-center">Coins</v-card-title>
+    <v-card-title class="justify-center">Coins {{ coin }}</v-card-title>
     <v-col>
+      <v-card-actions class="justify-center mt-4">
+        <v-autocomplete 
+        v-model="coins" 
+        :items="coinList"
+        item-text="name"
+        item-value="id"
+        label="Search for a coin"
+        outlined
+        clearable
+        small-chips
+        deletable-chips
+        multiple
+        @change="coinQueries(coins)"
+        ></v-autocomplete>
+      </v-card-actions>
       <v-card-actions class="justify-center mt-4">
         <v-btn-toggle
         v-model="chartSelectedDate"
@@ -15,7 +30,7 @@
       </v-card-actions>
       <v-card-actions class="justify-center mt-4">
         <div>
-          <LineChart :data="lineChartData" :options="lineChartOptions" :height="400"/>
+          <LineChart :data="lineChartData" :options="lineChartOptions" :height="500" :width="700"/>
         </div>
       </v-card-actions>
     </v-col>
@@ -23,7 +38,7 @@
 </template>
 
 <script>
-import LineChart from '@/components/LineChart.vue'
+import LineChart from '@/components/LineChart.vue';
 
 export default {
   components: {
@@ -31,67 +46,9 @@ export default {
   },
   data () {
     return {
-      lineChartData: {
-        labels: [
-          "2019-06",
-          "2019-07",
-          "2019-08",
-          "2019-09",
-          "2019-10",
-          "2019-11",
-          "2019-12",
-          "2020-01",
-          "2020-02",
-          "2020-03",
-        ],
-        datasets: [
-          {
-            label: "Visualizaciones",
-            data: [2, 1, 16, 3, 4, 5, 0, 0, 4, 12, 2],
-            backgroundColor: "rgba(20, 255, 0, 0.3)",
-            borderColor: "rgba(100, 255, 0, 1)",
-            borderWidth: 2,
-          },
-        ],
-      },
-      lineChartOptions: {
-        responsive: true,
-        legend: {
-          display: true,
-        },
-        title: {
-          display: true,
-          text: "Google analytics data",
-          fontSize: 24,
-          fontColor: "#6b7280",
-        },
-        tooltips: {
-          backgroundColor: "#17BF62",
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: true,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                max: 7,
-                min: 0,
-                stepSize: 1,
-              },
-              gridLines: {
-                display: true,
-              },
-            },
-          ],
-        },
-      },
-    };
+      coinList: [],
+      coins: ['bitcoin', ],
+    }
   }
 }
 </script>
--- components/LineChart.vue
+++ components/LineChart.vue
@@ -1,11 +1,85 @@
 <script>
+/* eslint-disable vue/require-prop-types */
 import { Line } from "vue-chartjs";
+import axios from 'axios';
 
 export default {
   extends: Line,
-  props: ["data", "options"],
+  props: ['data', 'options'],
+  data () {
+  return {
+    coinList: [],
+    coins: ['bitcoin', ],
+    lineChartData: {
+      labels: [],
+      datasets: [
+        {
+          label: "Cryptocurrency",
+          data: [],
+          backgroundColor: "rgba(20, 255, 0, 0.3)",
+          borderColor: "rgba(100, 255, 0, 1)",
+          borderWidth: 2,
+        },
+      ],
+    },
+    lineChartOptions: {
+      responsive: true,
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+        text: "Google analytics data",
+        fontSize: 24,
+        fontColor: "#6b7280",
+      },
+      tooltips: {
+        backgroundColor: "#17BF62",
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              display: true,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              max: 20,
+              min: 0,
+              stepSize: 1,
+            },
+            gridLines: {
+              display: true,
+            },
+          },
+        ],
+      },
+    },
+    }
+  },
+    async created() {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/list')
+    if(res.status === 200) {
+      res.data.forEach(element => {
+        this.coinList.push(element)
+      })
+    }
+  },
   mounted() {
     this.renderChart(this.data, this.options)
+  },
+  methods: {
+    async coinQueries(coins) {
+      const queries = coins.map(coin => axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=${this.chartSelectedDate}&interval=${this.chartInterval}`))
+      await axios.all(queries).then(axios.spread((...responses) => {
+        const prices = responses.map(res =>  res.data.prices)
+        this.coinMatrix = prices.map((p, i) => ['Date', coins[i]].concat(p))
+      }))
+    }
   }
-};
+}
 </script>
