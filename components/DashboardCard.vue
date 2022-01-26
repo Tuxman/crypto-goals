662f9f80ebd20eea969df75baad21b389d3c302d
jonbiveo <logantedmonds@gmail.com>
2022-01-26 13:48:57 -0600

    Sample chart added on dashboard

--- components/DashboardCard.vue
+++ components/DashboardCard.vue
@@ -2,12 +2,20 @@
   <div>
     <h1 class="ma-6">Dashboard</h1>
     <v-card>
+      <v-card class="ma-5">
+        <v-card-actions>
+          <div>
+            <DashLineChart/>
+          </div>
+        </v-card-actions>
+      </v-card>
     </v-card>
   </div>
 </template>
 
 <script>
+import DashLineChart from "./DashLineChart.vue";
 export default {
-
+    components: { DashLineChart }
 }
 </script>
